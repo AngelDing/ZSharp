427cc9893395025a2d662a85cdb897e89d642dc2
jiajiazhou <jiajiazhou@ctrip.com>
2015-10-22 14:26:32 +0800

    RabbitMq

---
+++ Framework/ZSharp.Framework.Domain/Implementation/RabbitMq/RabbitMqMessageReceiver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ZSharp.Framework.Domain
+{
+    public class RabbitMqMessageReceiver : MessageReceiver
+    {
+        public RabbitMqMessageReceiver()
+            : base(TimeSpan.FromMilliseconds(100))
+        {
+        }
+
+        protected override bool ReceiveMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Framework/ZSharp.Framework.Domain/Implementation/RabbitMq/RabbitMqMessageSender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZSharp.Framework.Domain
+{
+    public class RabbitMqMessageSender : MessageSender
+    {
+        public override void Send(IEnumerable<Message> messages)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Send(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
