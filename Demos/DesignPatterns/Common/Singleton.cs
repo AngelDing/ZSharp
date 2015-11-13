6da079cc1f48e6365df10e37bfda1f291c8fac94
jiajiazhou <jiajiazhou@ctrip.com>
2015-11-13 14:35:14 +0800

    Common

---
+++ Demos/DesignPatterns/Common/Singleton.cs
@@ -0,0 +1,37 @@
+﻿namespace Common
+{
+    /// <summary>
+    /// 標示對象是單實例的
+    /// </summary>
+    public interface ISingleton
+    {
+    }
+
+    public class Singleton : ISingleton
+    {
+        private static Singleton instance;
+        private static readonly object syncRoot = new object();
+
+        // the constructor should be protected or private
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            // double-check locking
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        // use lazy initialization
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
