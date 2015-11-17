364b927b555e0b7902591acd38c2730fc764d40a
jiajiazhou <jiajiazhou@ctrip.com>
2015-11-17 16:32:13 +0800

    BehavioralPatterns

---
+++ Demos/DesignPatterns/Common/BehavioralPatterns/Interpreter.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// 解析器（不常用） ： 给定一个语言，定义它的文法的一种表示，并定义一个解释器，这个解释器使用该表示来解释语言中的句子。
+/// 这个Pattern用在Compiler的 Language Parser最合适。他可以反覆递归，最终分析好一个语言。
+/// 语法就象: 算术表达式 ＝ 算术表达式 ＋／－？＊／／ 数字
+/// 然后构造一个算术表达式类和数字类，在算术表达式类中递归引用自己和数字类。最终能够解开任何的语言。
+/// 在Gang Of Four的网页上是通过LinkedList来处理这个例子。 LinkedList就是先准备好一堆Class，
+/// 一个一个拿过来用在Context上。比较死板，只能处理很少的情况。在Compiler中最常用的是递归。
+/// 反复递归就是有一万层括号也能解开，但是用LinkedList就会受限制了。"
+/// </summary>
+namespace Common.BehavioralPatterns
+{
+    /// <summary>
+    /// 表示所有表达式的抽象接口
+    /// </summary>
+    public interface IExpression
+    {
+        /// <summary>
+        /// 用Context负责保存中间结果
+        /// </summary>
+        /// <param name="ctx"></param>
+        object Interpret(IContext ctx);
+    }
+
+    /// <summary>
+    /// 用于保存解析过程的中间结果以及当前执行的操作符
+    /// </summary>
+    public interface IContext
+    {
+    }
+}
---
+++ Demos/DesignPatterns/Common/BehavioralPatterns/TemplateMethod.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary>
+/// 模板方法： 定义一个操作中的算法的骨架，而将一些步骤延迟到子类中。
+///          此模式使得子类可以不改变一个算法的结构即可重定义该算法的某些特定步骤。
+/// </summary>
+namespace Common.BehavioralPatterns
+{
+    /// <summary>
+    /// 模板方法标示
+    /// </summary>
+    public interface ITemplateMethod
+    {
+    }
+
+    /// <summary>
+    /// 抽象的模版接口
+    /// </summary>
+    public interface IAbstract : ITemplateMethod
+    {
+        int Quantity { get; }
+
+        double Total { get; }
+
+        double Average { get; }
+    }
+
+    /// <summary>
+    /// 定义了算法梗概的抽象类型
+    /// </summary>
+    public abstract class AbstractBase : IAbstract
+    {
+        public abstract int Quantity { get; }
+
+        public abstract double Total { get; }
+
+        /// <summary>
+        /// 算法梗概
+        /// </summary>
+        public virtual double Average { get { return Total / Quantity; } }
+    }
+
+    /// <summary>
+    /// 具体类型
+    /// </summary>
+    public class ArrayData : AbstractBase
+    {
+        protected double[] data = new double[3] { 1.1, 2.2, 3.3 };
+
+        public override int Quantity { get { return data.Length; } }
+
+        public override double Total
+        {
+            get
+            {
+                double count = 0;
+                for (int i = 0; i < data.Length; i++) count += data[i];
+                return count;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 具体类型
+    /// </summary>
+    public class ListData : AbstractBase
+    {
+        protected IList<double> data = new List<double>();
+
+        public ListData() { data.Add(1.1); data.Add(2.2); data.Add(3.3); }
+
+        public override int Quantity { get { return data.Count; } }
+
+        public override double Total
+        {
+            get
+            {
+                double count = 0;
+                foreach (double item in data) count += item;
+                return count;
+            }
+        }
+    }
+
+    public class TemplateMethodClient
+    {
+        public void Test()
+        {
+            IAbstract i1 = new ArrayData();
+            Console.WriteLine(i1.Average);
+
+            IAbstract i2 = new ListData();
+            Console.WriteLine(i2.Average);
+        }
+    }
+}
