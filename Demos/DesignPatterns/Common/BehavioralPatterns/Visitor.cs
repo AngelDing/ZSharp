4c94fbdcbfe037709059cc7d687d428900147e1b
jiajiazhou <jiajiazhou@ctrip.com>
2015-11-20 17:44:47 +0800

    Visitor

---
+++ Demos/DesignPatterns/Common/BehavioralPatterns/Visitor.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// http://www.cnblogs.com/gaochundong/p/design_pattern_visitor.html
+/// http://www.cnblogs.com/java-my-life/archive/2012/05/16/2502279.html
+/// </summary>
+
+namespace Common.BehavioralPatterns
+{
+    class Visitor
+    {
+    }
+}
