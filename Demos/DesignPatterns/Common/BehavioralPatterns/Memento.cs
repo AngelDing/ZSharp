bba16da6196afbed889eb1961c9a7155136a0af1
jiajiazhou <jiajiazhou@ctrip.com>
2015-11-20 14:10:48 +0800

    MementoPattern

--- Demos/DesignPatterns/Common/BehavioralPatterns/Memento.cs
+++ Demos/DesignPatterns/Common/BehavioralPatterns/Memento.cs
@@ -1,12 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+/// <summary>
+/// 备忘录模式又叫做快照模式(Snapshot Pattern)或Token模式，是对象的行为模式。
+/// 备忘录对象是一个用来存储另外一个对象内部状态的快照的对象。备忘录模式的用意是在不破坏封装的条件下，
+/// 将一个对象的状态捕捉(Capture)住，并外部化，存储起来，从而可以在将来合适的时候把这个对象还原到存储起来的状态。
+/// 备忘录模式常常与命令模式和迭代子模式一同使用。
+/// </summary>
 
 namespace Common.BehavioralPatterns
 {
-    class Memento
+    /// <summary>
+    /// 为了便于定义抽象状态类型所定义的接口
+    /// </summary>
+    public interface ISnapshoot { }
+
+    /// <summary>
+    /// 抽象备忘录对象接口
+    /// </summary>
+    public interface IMemento<T> where T : ISnapshoot
     {
+        T GetSnapshoot();
+
+        void SetSnapshoot(T snapshoot);
     }
+
+    /// <summary>
+    /// 快照可以存在内存中，也可以持久化到db中，
+    /// 如果存入db中，则可以叫做IMementoStore，或者IMementoRepository
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class MementoBase<T> : IMemento<T>
+        where T : ISnapshoot
+    {
+        private T snapshoot;
+
+        public virtual T GetSnapshoot()
+        {
+            return this.snapshoot;
+        }
+
+        public virtual void SetSnapshoot(T snapshoot)
+        {
+            this.snapshoot = snapshoot;
+        }
+    }
+
+    /// <summary>
+    /// 抽象的发起人对象接口
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// /// <typeparam name="M"></typeparam>
+    public interface IOriginator<T, M>
+        where T : ISnapshoot
+        where M : IMemento<T>, new()
+    {
+        IMemento<T> Memento { get; }
+    }
+
+    public abstract class OriginatorBase<T, M>
+        where T : ISnapshoot
+        where M : IMemento<T>, new()
+    {
+        /// <summary>
+        /// 发起人对象的状态
+        /// </summary>
+        protected T state;
+
+        /// <summary>
+        /// 把状态保存到备忘录，或者从备忘录恢复之前的状态
+        /// </summary>
+        public virtual IMemento<T> Memento
+        {
+            get
+            {
+                M m = new M();
+                m.SetSnapshoot(this.state);
+                return m;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException();
+                this.state = value.GetSnapshoot();
+            }
+        }
+    }   
 }
---
+++ Demos/DesignPatterns/MementoPattern/MementoClient.cs
@@ -0,0 +1,57 @@
+﻿using Common.BehavioralPatterns;
+using System;
+
+namespace MementoPattern
+{
+    /// <summary>
+    /// 具体状态类型
+    /// </summary>
+    public struct Position : ISnapshoot
+    {
+        public int X;
+        public int Y;
+    }
+
+    /// <summary>
+    /// 具体备忘录类型
+    /// </summary>
+    public class Memento : MementoBase<Position> { }
+
+    /// <summary>
+    /// 具体发起人类型
+    /// </summary>
+    public class Originator : OriginatorBase<Position, Memento>
+    {
+        /// <summary>
+        /// 供客户程序使用的非备忘录相关操作
+        /// </summary>
+        /// <param name="x"></param>
+        public void UpdateX(int x) { base.state.X = x; }
+        public void DecreaseX() { base.state.X--; }
+        public void IncreaseY() { base.state.Y++; }
+        public Position Current { get { return base.state; } }
+    }
+
+    public class MementoClient
+    {
+        public void Test()
+        {
+            Originator originator = new Originator();
+            Console.WriteLine(originator.Current.Y);  //0
+            Console.WriteLine(originator.Current.X);  //0
+            // 保存发起人刚初始化后的状态
+            IMemento<Position> m1 = originator.Memento;
+
+            // 对发起人进行操作，验证状态的修改
+            originator.IncreaseY();
+            originator.DecreaseX();
+            Console.WriteLine(originator.Current.Y);  //1
+            Console.WriteLine(originator.Current.X);  //-1
+
+            // 确认备忘录的恢复作用
+            originator.Memento = m1;
+            Console.WriteLine(originator.Current.Y);  //0
+            Console.WriteLine(originator.Current.X);  //0
+        }
+    }
+}
